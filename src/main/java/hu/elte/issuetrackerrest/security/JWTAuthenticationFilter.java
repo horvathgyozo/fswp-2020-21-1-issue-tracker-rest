020dd578a742d52b46cc3c41ed7045abae93c00f
Győző Horváth <gyozo.horvath@inf.elte.hu>
2020-10-08 14:49:59 +0200

    POST auth with in-memory users

---
+++ src/main/java/hu/elte/issuetrackerrest/security/JWTAuthenticationFilter.java
@@ -0,0 +1,71 @@
+package hu.elte.issuetrackerrest.security;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.stream.Collectors;
+import javax.servlet.FilterChain;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+    private AuthenticationManager authenticationManager;
+    
+    private static final String COOKIE_NAME = "token";
+
+    private static final int EXPIRATION =  30 * 60 * 1000;
+    
+    private static final String SECRET =  "alma";
+
+    public JWTAuthenticationFilter(AuthenticationManager authenticationManager) {
+        this.authenticationManager = authenticationManager;
+        setFilterProcessesUrl("/api/auth"); 
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest req,
+                                                HttpServletResponse res) throws AuthenticationException {
+        try {
+            UserCredentials creds = new ObjectMapper()
+                    .readValue(req.getInputStream(), UserCredentials.class);
+
+            return authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(
+                            creds.getUsername(),
+                            creds.getPassword(),
+                            new ArrayList<>())
+            );
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest req,
+                                            HttpServletResponse res,
+                                            FilterChain chain,
+                                            Authentication auth) throws IOException {
+        long now = System.currentTimeMillis();
+        String token = Jwts.builder()
+                    .setSubject(auth.getName())
+                    .claim("authorities", auth.getAuthorities().stream()
+                            .map(GrantedAuthority::getAuthority).collect(Collectors.toList()))
+                    .setIssuedAt(new Date(now))
+                    .setExpiration(new Date(now + EXPIRATION))
+                    .signWith(SignatureAlgorithm.HS512, SECRET)
+                    .compact();
+
+        res.getWriter().write(token);
+        res.getWriter().flush();
+    }
+}
---
+++ src/main/java/hu/elte/issuetrackerrest/security/WebSecurityConfig.java
@@ -0,0 +1,74 @@
+package hu.elte.issuetrackerrest.security;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+//    private static void handleException(HttpServletRequest req, HttpServletResponse rsp, AuthenticationException e)
+//    throws IOException {
+//        PrintWriter writer = rsp.getWriter();
+//        writer.println(new ObjectMapper().writeValueAsString("Unauthorized"));
+//        rsp.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+//    }
+    
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http    
+//                .cors().and()
+                .csrf().disable()
+                .authorizeRequests()
+                    .antMatchers(HttpMethod.POST, "/api/auth").permitAll()
+                    .anyRequest().authenticated()
+                    .and()
+//                .exceptionHandling().authenticationEntryPoint(WebSecurityConfig::handleException)
+//                    .and()
+                .addFilter(new JWTAuthenticationFilter(authenticationManager()))
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+//                .addFilter(new JwtUsernameAndPasswordAuthenticationFilter(jwtCookieStore, authenticationManager()))
+//                .addFilterAfter(new JwtTokenAuthenticationFilter(jwtCookieStore), UsernamePasswordAuthenticationFilter.class)
+                
+    }
+
+    @Autowired
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .inMemoryAuthentication()
+                .withUser("user")
+//                .password(passwordEncoder().encode("user"))
+                .password("{noop}user")
+                .authorities("ROLE_USER");
+    }
+
+//    @Bean
+//    public BCryptPasswordEncoder passwordEncoder() {
+//        return new BCryptPasswordEncoder();
+//    }
+    
+//    @Bean
+//    CorsConfigurationSource corsConfigurationSource() {
+//      final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//      source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+//      return source;
+//    }
+
+}
